354e6c0f4ea930db4cc457510396cfb448ceaf07
LucasMarquesdeSousa <marqueslukas66@gmail.com>
2021-08-14 09:01:44 -0300

    Primeiro Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './usuario/login/login.component';
+import { CadastarComponent } from './usuario/cadastar/cadastar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastrar', component:CadastarComponent} ,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import  { AppRoutingModule } from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { CadastarComponent } from './usuario/cadastar/cadastar.component';
+import { LoginComponent } from './usuario/login/login.component';
+const appRoutes: Routes = [
+  { path: '', component: HeaderComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CadastarComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
